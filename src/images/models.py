b3803802795021dfb203723192ce2bf7e4ff525a
amir ghazanfari <amir_ghazanfari@outlook.com>
2020-10-08 20:31:25 +0330

    adding api link to project2

--- src/images/models.py
+++ src/images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    picture = models.ImageField()
+    classify = models.CharField(max_length=100, blank=True)
+    uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'image classified at {}'.format(self.uploaded.strftime('%Y/%m/%d %H:%M'))
