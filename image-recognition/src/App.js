0c2c3fd6316d2366a5bc7134e4ada4a3be315be2
amir ghazanfari <amir_ghazanfari@outlook.com>
2020-10-09 23:06:55 +0330

    reinstall react and adding classifier components

---
+++ image-recognition/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Classifier from './Components/Classifier/Classifier'
+
+function App() {
+  return (
+    <div className="APP">
+      <Classifier/>
+    </div>
+  );
+}
+
+export default App;
